7728e1ded4af75b293d552063702164fa2f53480
Christian Camacho <chris.ca.dev@gmail.com>
2023-06-19 15:16:18 -0700

    update lists

--- scripts/allow-list.sh
+++ scripts/allow-list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo /scripts/log.sh INFO "allowing entretaiment"
-sudo /scripts/remove_from_txt.sh /url-lists/entretaiment.list
+sudo /scripts/remove_from_txt.sh "$1"
 sudo /scripts/rc.sh
 sudo /scripts/log.sh INFO "entretaiment allowed"
--- scripts/block-list.sh
+++ scripts/block-list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo /scripts/log.sh INFO "blocking entretaiment"
-sudo /scripts/add_from_txt.sh /url-lists/entretaiment.list
+sudo /scripts/add_from_txt.sh "$1"
 sudo /scripts/rc.sh
 sudo /scripts/log.sh INFO "entretaiment bloked"
